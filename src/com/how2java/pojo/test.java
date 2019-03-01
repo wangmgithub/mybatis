c656eb166a41bb1b0fcd7b753d089f06fe1a2da1
wangmgithub <1182808225@qq.com>
2019-03-01 13:52:21 +0800

    test

--- src/com/how2java/pojo/test.java
+++ src/com/how2java/pojo/test.java
@@ -5,4 +5,9 @@ package com.how2java.pojo;
  * @date:2019/3/1 0001 下午 1:14
  */
 public class test {
+    public static void main(String args[]) {
+            System.out.println("");
+        System.out.println("wwwwww");
+        }
+
 }
